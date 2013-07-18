664886d0828c7e489c4b36128f358bee828e0fe7
user3368 <user3368@74-80-239-54.ic2net.net>
2013-07-18 06:04:07 -0700

    checkpoint

---
+++ meetup/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  meetup
+//
+//  Created by user3368 on 7/18/13.
+//  Copyright (c) 2013 Chris League. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
